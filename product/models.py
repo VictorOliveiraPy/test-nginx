6e461d1086a3482955f30c0bf3053f12b1f87b8c
Ricardo da Silva Reis <oricardo_@hotmail.com>
2021-06-14 13:56:44 -0300

    Created model Category

--- product/models.py
+++ product/models.py
@@ -15,3 +15,12 @@ class Product(BaseModel):
     gtin = models.CharField(max_length=30)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return f"{self.name} - {self.descripiton} - {self.price} - {self.stock} - {self.gtin} - {self.created_at} - {self.updated_at} - {self.category}"
+
+class Category(BaseModel):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.description} - {self.created_at} - {self.updated_at}"
