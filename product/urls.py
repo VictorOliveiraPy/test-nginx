df01ed271d3bbcc535a76ff4c0e5202a3241ef0d
JuniorCardoso <tarcisio.cardoso@olist.com>
2021-06-14 16:37:20 -0300

    created views crud

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.list_categories, name='list_categories'),
+    path('create/', views.create_categories, name='create_categories'),
+    path('delete/<int:id>', views.delete_categories, name='delete_category'),
+    path('update/<int:id>', views.update_categories, name='update_category'),
 ]
+
