895847d815c15b589052844033c5a2898401f40a
JuniorCardoso <tarcisio.cardoso@olist.com>
2021-06-15 15:01:30 -0300

    created tests to models product and views

--- product/models.py
+++ product/models.py
@@ -11,20 +11,21 @@ class BaseModel(models.Model):
 
 class Product(BaseModel):
     name = models.CharField(max_length=200)
-    descripiton = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
     price = models.FloatField()
     stock = models.IntegerField()
     gtin = models.CharField(max_length=30)
-    category = models.ManyToManyField('Category')
+    category = models.ManyToManyField('Category',verbose_name="list of categories")
 
     def __str__(self) -> str:
-        return f"{self.name} - {self.descripiton} - {self.price} - {self.stock} - {self.gtin} - {self.created_at} - {self.updated_at}"
+        return f"{self.name} - {self.description} - {self.price} - {self.stock} - {self.gtin} - {self.created_at} - {self.updated_at}"
+
 
     def get_all_categories(self):
         category = ''
         for cat in self.category.all():
             category += f' {cat},'
-        return category
+        return category.strip()
 
 
 class Category(BaseModel):
